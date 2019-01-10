c55d39fef3b99fb8d157e0e6b66261c8c428f117
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-01-09 22:26:32 -0800

    Added Bootstrap CSS

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { 
+    Container
+} from 'reactstrap';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Container>
+                <h1>Hello World!</h1>
+            </Container>
+        );
+    }
+}
+
+export default App;
