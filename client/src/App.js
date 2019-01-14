1301dad45b360b23d867bb84452952c63d357344
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-01-13 22:40:23 -0800

    Implemented Redux reducer, action, created action types, and routed the Redux state to ItemList component.

--- client/src/App.js
+++ client/src/App.js
@@ -4,47 +4,22 @@ import {
 } from 'reactstrap';
 import AddTodo from './components/AddTodo';
 import ItemList from './components/ItemList';
-import uuid from 'uuid';
 import './css/index.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            items: [
-                {
-                    id: uuid(),
-                    todo: "Make this app."
-                },
-                {
-                    id: uuid(),
-                    todo: "Find a job."
-                },
-                {
-                    id: uuid(),
-                    todo: "Seek fulfillment."
-                },
-            ]
-        }
-    }
-
-    add = () => {
-        let todo = document.getElementById('new-todo').value;
-        if (todo) {
-            this.setState(state => ({
-                items: [...state.items, { id: uuid(), todo}]
-            }));
-        }
-    }
 
     render() {
         return (
-            <Container id="main-container">
-                <h1 id="header">Todo List</h1>
-                <AddTodo add={ this.add } />
-                <ItemList state={ this.state } />
-            </Container>
+            <Provider store={ store }>
+                <Container id="main-container">
+                    <h1 id="header">Todo List</h1>
+                    <AddTodo />
+                    <ItemList state={ this.state } />
+                </Container>
+            </Provider>
         );
     }
 }
--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -5,6 +5,9 @@ import {
     Button
 } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ItemListItem extends React.Component {
     constructor(props) {
@@ -18,6 +21,12 @@ class ItemListItem extends React.Component {
         this.setState({
             checked: !this.state.checked
         });
+    }
+
+    delete = (id) => {
+        this.setState(state => ({
+            items: state.items.filter(item => item.id !== id)
+        }));
     }
 
     render() {
@@ -38,7 +47,7 @@ class ItemListItem extends React.Component {
                     className="float-right"
                     color="danger"
                     size="sm"
-                    onClick={ this.props.delete }
+                    onClick={() =>  this.delete(this.props.lookup) }
                 >&times;</Button>
             </ListGroupItem>
         );
@@ -47,20 +56,21 @@ class ItemListItem extends React.Component {
 
 class ItemList extends React.Component {
 
+    componentDidMount() {
+        this.props.getItems();
+    }
+
     render() {
-        const { items } = this.props.state;
+        const { items } = this.props.item;
         return (
             <ListGroup>
                 <TransitionGroup className="todo-list">
                     {items.map(({ id, todo }) => (
-                        <CSSTransition key={ id } timeout={ 500 } classNames="fade">
+                        <CSSTransition key={ id } timeout={ 1000 } classNames="fade">
                             <ItemListItem
+                                lookup={ id }
                                 todo={ todo }
-                                delete={() => {
-                                    this.setState(state => ({
-                                        items: state.items.filter(item => item.id !== id)
-                                    }));
-                                }}
+                                delete={ this.props.delete }
                             />
                         </CSSTransition>
                     ))}
@@ -70,4 +80,13 @@ class ItemList extends React.Component {
     }
 }
 
-export default ItemList;
+ItemList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+});
+
+export default connect(mapStateToProps, { getItems })(ItemList);
