7facc19ae0bdfc8065cf43d6f438a55cbadd2094
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-01-19 20:43:31 -0800

    Added expected functionality to todo input and submit.

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -2,12 +2,31 @@ import React from 'react';
 import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap';
 
 class AddTodo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({value: e.target.value});
+    }
+
+    submit = (e) => {
+        if (e.key === 'Enter' || e.type === 'click') {
+            this.props.add();
+            this.setState({value: ''});
+        }
+    }
 
     render() {
         return (
             <InputGroup className="w-50 my-3 mx-auto">
-                <Input type="text" id="new-todo" placeholder="Enter new todo..." />
-                <InputGroupAddon addonType="append"><Button onClick={ this.props.add }>Submit</Button></InputGroupAddon>
+                <Input type="text" id="new-todo" placeholder="Enter new todo..." value={this.state.value} onChange={this.handleChange} onKeyPress={this.submit} />
+                <InputGroupAddon addonType="append">
+                    <Button onClick={this.submit}>Submit</Button>
+                </InputGroupAddon>
             </InputGroup>
         );
     }
